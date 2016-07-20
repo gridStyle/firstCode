7362530318d8505bf8d0da484feaa20eb0526a0e
gridStyle <627380713@qq.com>
2016-07-20 19:37:46 +0800

    02.update_0720_lxd

--- gen/com/example/activitytest/R.java
+++ gen/com/example/activitytest/R.java
@@ -23,6 +23,7 @@ public final class R {
     public static final class layout {
         public static final int first_layout=0x7f030000;
         public static final int second_layout=0x7f030001;
+        public static final int third_layout=0x7f030002;
     }
     public static final class menu {
         public static final int main=0x7f060000;
--- src/com/example/activitytest/FirstActivity.java
+++ src/com/example/activitytest/FirstActivity.java
@@ -2,6 +2,7 @@ package com.example.activitytest;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -28,8 +29,12 @@ public class FirstActivity extends Activity{
 						Toast.LENGTH_SHORT).show();*/
 				/*Intent intent = new Intent(FirstActivity.this, SecondActivity.class);
 				startActivity(intent);*/
-				Intent intent = new Intent("com.example.activitytest.ACTION_START");
-				intent.addCategory("com.example.activitytest.MY_CATEGORY");
+//				Intent intent = new Intent("com.example.activitytest.ACTION_START");
+//				intent.addCategory("com.example.activitytest.MY_CATEGORY");
+				
+				Intent intent = new Intent(Intent.ACTION_VIEW);
+				intent.setData(Uri.parse("http://www.baidu.com"));
+				
 				startActivity(intent);
 			}
 		});
